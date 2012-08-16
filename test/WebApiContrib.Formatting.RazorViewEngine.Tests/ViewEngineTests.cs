b62a0909a9a35721d499d3b93b651a714f458cde
Ryan Riley <ryan.riley@panesofglass.org>
2012-08-16 15:57:48 -0500

    Use the concurrency-friendly RazorEngine API and add additional overloads to RazorViewEngine to assist with use.

--- test/WebApiContrib.Formatting.RazorViewEngine.Tests/ViewEngineTests.cs
+++ test/WebApiContrib.Formatting.RazorViewEngine.Tests/ViewEngineTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using NUnit.Framework;
-using WebApiContrib.Formatting.RazorViewEngine;
+using RazorEngine.Configuration;
+using RazorEngine.Templating;
 
 namespace WebApiContrib.Formatting.RazorViewEngine.Tests
 {
@@ -51,7 +52,31 @@ namespace WebApiContrib.Formatting.RazorViewEngine.Tests
             Assert.AreEqual("<html>Hello foo! Welcome to Razor!</html>", output);
         }
 
-        private MemoryStream GetStreamFromString(string helloModelNameWelcomeToRazor)
+        [Test]
+        public void render_template_with_specified_resolver()
+        {
+            MemoryStream templateStream = GetStreamFromString(@"@{_Layout = ""~/Embed.cshtml"";}Hello @Model.Name! Welcome to Razor!");
+
+            var outputStream = new MemoryStream();
+
+	        var resolver = new EmbeddedResolver(this.GetType());
+
+            var formatter = new ViewEngineFormatter(new RazorViewEngine(resolver));
+
+            var view = new View(templateStream, new { Name = "foo" });
+
+            var task = formatter.WriteToStreamAsync(typeof(View), view, outputStream, null, null);
+
+            task.Wait();
+
+            outputStream.Position = 0;
+
+            var output = new StreamReader(outputStream).ReadToEnd();
+
+            Assert.AreEqual("<html>Hello foo! Welcome to Razor!</html>", output);
+        }
+
+        private static MemoryStream GetStreamFromString(string helloModelNameWelcomeToRazor)
         {
             var templateStream = new MemoryStream();
             var sr = new StreamWriter(templateStream);
