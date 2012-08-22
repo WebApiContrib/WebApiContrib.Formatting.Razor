77f7ee161e997fd9474097f63dba38a8cbd44722
Ryan Riley <ryan.riley@panesofglass.org>
2012-08-21 20:53:51 -0500

    Fix test file whitespace.

--- test/WebApiContrib.Formatting.RazorViewEngine.Tests/ViewEngineTests.cs
+++ test/WebApiContrib.Formatting.RazorViewEngine.Tests/ViewEngineTests.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-using System.Net.Http;
-using System.Reflection;
+﻿using System.Net.Http;
 using NUnit.Framework;
 using WebApiContrib.Formatting.Html;
 using WebApiContrib.Formatting.Html.Configuration;
@@ -12,23 +10,23 @@ namespace WebApiContrib.Formatting.RazorViewEngine.Tests
     [TestFixture]
     public class ViewEngineTests
     {
-	    private HtmlMediaTypeViewFormatter _formatter;
+        private HtmlMediaTypeViewFormatter _formatter;
 
-		[SetUp]
-		public void Before()
-		{
-			_formatter = new HtmlMediaTypeViewFormatter();
+        [SetUp]
+        public void Before()
+        {
+            _formatter = new HtmlMediaTypeViewFormatter();
             GlobalViews.DefaultViewParser = new RazorViewParser();
             GlobalViews.DefaultViewLocator = new RazorViewLocator();
-		}
+        }
 
         [Test]
         public void render_simple_template()
         {
             var view = new View("Test1", new {Name = "foo"});
-	        var content = new ObjectContent<View>(view, _formatter);
+            var content = new ObjectContent<View>(view, _formatter);
 
-	        var output = content.ReadAsStringAsync().Result;
+            var output = content.ReadAsStringAsync().Result;
 
             Assert.AreEqual("Hello foo! Welcome to Razor!", output);
         }
@@ -37,9 +35,9 @@ namespace WebApiContrib.Formatting.RazorViewEngine.Tests
         public void render_template_with_embedded_layout()
         {
             var view = new View("Test2", new { Name = "foo" });
-	        var content = new ObjectContent<View>(view, _formatter);
+            var content = new ObjectContent<View>(view, _formatter);
 
-	        var output = content.ReadAsStringAsync().Result;
+            var output = content.ReadAsStringAsync().Result;
 
             Assert.AreEqual("<html>Hello foo! Welcome to Razor!</html>", output);
         }
@@ -47,12 +45,12 @@ namespace WebApiContrib.Formatting.RazorViewEngine.Tests
         [Test]
         public void render_template_with_specified_resolver()
         {
-	        var resolver = new EmbeddedResolver(this.GetType());
+            var resolver = new EmbeddedResolver(this.GetType());
             var formatter = new HtmlMediaTypeViewFormatter(null, new RazorViewLocator(), new RazorViewParser(resolver));
             var view = new View("Test2", new { Name = "foo" });
-	        var content = new ObjectContent<View>(view, formatter);
+            var content = new ObjectContent<View>(view, formatter);
 
-	        var output = content.ReadAsStringAsync().Result;
+            var output = content.ReadAsStringAsync().Result;
 
             Assert.AreEqual("<html>Hello foo! Welcome to Razor!</html>", output);
         }
