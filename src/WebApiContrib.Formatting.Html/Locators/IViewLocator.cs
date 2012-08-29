c0066a37b6d3b23a481130a4ad10a30faf864fd9
Ryan Riley <ryan.riley@panesofglass.org>
2012-08-29 10:26:53 -0500

    Replace direct reference to the Formatting.Html project to the NuGet package.

--- src/WebApiContrib.Formatting.Html/Locators/IViewLocator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebApiContrib.Formatting.Html.Locators
-{
-    public interface IViewLocator
-    {
-        string GetView(string siteRootPath, IView view);
-    }
-}
--- src/WebApiContrib.Formatting.Html/ViewParsers/IViewParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text;
-
-namespace WebApiContrib.Formatting.Html.ViewParsers
-{
-    public interface IViewParser
-    {
-        byte[] ParseView(IView view, string viewTemplate, Encoding encoding);
-    }
-}
