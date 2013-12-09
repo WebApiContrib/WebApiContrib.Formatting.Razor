78ed919d4b584e36a167755231bdde942d09e030
Ryan Riley <ryan.riley@panesofglass.org>
2013-12-09 14:39:48 -0600

    Add RazorViewFormatter to ease creation of a Razor-based HtmlMediaTypeViewFormatter instance.

--- test/WebApiContrib.Formatting.Razor.Tests/ViewEngineTests.cs
+++ test/WebApiContrib.Formatting.Razor.Tests/ViewEngineTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Http;
 using NUnit.Framework;
 using WebApiContrib.Formatting.Html;
 using WebApiContrib.Formatting.Html.Formatting;
@@ -10,21 +11,32 @@ namespace WebApiContrib.Formatting.Razor.Tests
     [TestFixture]
     public class ViewEngineTests
     {
-        private HtmlMediaTypeViewFormatter _formatter;
+        private HttpRequestMessage _request;
 
         [SetUp]
         public void Before()
         {
-            _formatter = new HtmlMediaTypeViewFormatter();
-            GlobalViews.DefaultViewParser = new RazorViewParser();
-            GlobalViews.DefaultViewLocator = new RazorViewLocator();
+            var formatter = new RazorViewFormatter();
+            var config = new HttpConfiguration();
+            config.Formatters.Add(formatter);
+
+            _request = new HttpRequestMessage();
+            _request.SetConfiguration(config);
+            _request.RegisterForDispose(config);
+        }
+
+        [TearDown]
+        public void After()
+        {
+            _request.Dispose();
+            _request = null;
         }
 
         [Test]
         public async Task render_simple_template()
         {
             var cts = new CancellationTokenSource();
-            var view = new ViewResult(new HttpRequestMessage(), "Test1", new {Name = "foo"});
+            var view = new ViewResult(_request, "Test1", new {Name = "foo"});
 
             var response = await view.ExecuteAsync(cts.Token);
             var output = await response.Content.ReadAsStringAsync();
@@ -36,7 +48,7 @@ namespace WebApiContrib.Formatting.Razor.Tests
         public async Task render_template_with_embedded_layout()
         {
             var cts = new CancellationTokenSource();
-            var view = new ViewResult(new HttpRequestMessage(), "Test2", new { Name = "foo" });
+            var view = new ViewResult(_request, "Test2", new { Name = "foo" });
 
             var response = await view.ExecuteAsync(cts.Token);
             var output = await response.Content.ReadAsStringAsync();
@@ -50,7 +62,14 @@ namespace WebApiContrib.Formatting.Razor.Tests
             var cts = new CancellationTokenSource();
             var resolver = new EmbeddedResolver(this.GetType());
             var formatter = new HtmlMediaTypeViewFormatter(null, new RazorViewLocator(), new RazorViewParser(resolver));
-            var view = new ViewResult(new HttpRequestMessage(), "Test2", new { Name = "foo" });
+
+            // Replace the HTML formatter.
+            var config = _request.GetConfiguration();
+            var oldFormatter = config.Formatters.GetHtmlFormatter();
+            config.Formatters.Remove(oldFormatter);
+            config.Formatters.Add(formatter);
+
+            var view = new ViewResult(_request, "Test2", new { Name = "foo" });
 
             var response = await view.ExecuteAsync(cts.Token);
             var output = await response.Content.ReadAsStringAsync();
